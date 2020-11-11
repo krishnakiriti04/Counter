90eebd9a16239d46d5e8f41028f51cb7365486fe
krishna kireeti <krishnakiriti.mamidi@gmail.com>
2020-11-11 21:29:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Counter = ({count, increment, del})=>{
+	return (
+		<div>
+			<h4>{count}</h4>
+			<button onClick={()=>increment()}>Count</button>
+			<button onClick={()=>del()}>Delete</button>
+		</div>
+		)
 }
 
-export default App;
+const App = ()=>{
+	const [count,setCount] = useState([0]);
+
+	const addcounter=() => {
+		setCount(count.concat([0]))
+	}
+
+	const increment = (currInd)=>{
+		let newVal = count.map((val,ind)=>currInd===ind ? val = val+1: val );
+		setCount(newVal);
+	}
+
+	const del = (currInd)=>{
+		const newcount = count.filter((val,ind)=>ind!==currInd)
+		setCount(newcount);
+	}
+
+	return (
+	<div style={{textAlign :"center"}}>
+	{count.map((val,ind) => <Counter key= {ind} count={val} increment = {()=>increment(ind)} del={()=>del(ind) }/>)}
+		<br />
+		<button onClick={addcounter}>Add Counter</button>
+	</div>
+)}
+
+ export default App;
